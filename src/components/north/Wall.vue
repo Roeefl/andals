ab6b57413e02becc3e328eda8270daac115a3769
Roee FL <roeefl@gmail.com>
2020-04-30 21:25:23 +0300

    WallSection renderKey

--- src/components/north/Wall.vue
+++ src/components/north/Wall.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="the-wall">
-      <WallSection
-        v-for="(sectionStart, w) in wallTiles"
-        :key="`wall-${w}`"
-        :section="w"
-        :myColor="myColor"
-        :allowPurchase="allowPurchase"
-        :allowRemove="allowRemove"
-        :guards="wall.slice(sectionStart, sectionStart + 5)"
-        @wall-clicked="$emit('wall-clicked', { section: w, position: $event })"
-        @kill-guard="$emit('kill-guard', { section: w, position: $event })"
-        @relocate-guard="$emit('relocate-guard', { ...$event, toSection: w })"
-      />
+    <WallSection
+      v-for="(wallSection, w) in wallSections"
+      :key="renderKey(wallSection, w)"
+      :section="w"
+      :guards="wallSection"
+      :myColor="myColor"
+      :allowPurchase="allowPurchase"
+      :allowRemove="allowRemove"
+      @wall-clicked="$emit('wall-clicked', { section: w, position: $event })"
+      @kill-guard="$emit('kill-guard', { section: w, position: $event })"
+      @relocate-guard="$emit('relocate-guard', { ...$event, toSection: w })"
+    />
   </div>
 </template>
 
@@ -43,8 +43,22 @@
         default: false   
       }
     },
+    computed: {
+      wallSections: function() {
+        return wallTiles.map(sectionStart => this.wall.slice(sectionStart, sectionStart + 5));
+      }
+    },
     created() {
       this.wallTiles = wallTiles;
+    },
+    methods: {
+      renderKey(wallSection, sectionIndex) {
+        const allOwnerIds = wallSection
+          .map(guard => (guard || {}).ownerId)
+          .join('-');
+
+        return `wall-${sectionIndex}-${allOwnerIds}`;
+      }
     }
   }
 </script>
