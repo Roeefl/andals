95844510798b5750779d3b23f38009ae7322db19
Roee FL <roeefl@gmail.com>
2020-05-02 23:24:45 +0300

    Adjusted max wildling dice to 10

--- src/specs/dice.js
+++ src/specs/dice.js
@@ -1,7 +1,9 @@
 import colors from '@/styles/export.scss';
 
+export const WILDLING_DICE_MAX = 10;
+
 export const diceColors = {
   dice0: 'warning',
   dice1: 'success',
-  dice2: colors.mountain
+  dice2: colors.primary
 };
