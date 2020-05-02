8dbf6107f6cb5a46cf817767f87ea6884c9670b0
Roee FL <roeefl@gmail.com>
2020-05-02 11:56:00 +0300

    Added vuejs-dialog package

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import colors from 'vuetify/lib/util/colors';
+import VuejsDialog from 'vuejs-dialog';
+import VuejsDialogMixin from 'vuejs-dialog/dist/vuejs-dialog-mixin.min.js';
+import 'vuejs-dialog/dist/vuejs-dialog.min.css';
 
 Vue.use(Vuetify);
+
+Vue.use(VuejsDialog, {
+  okText: 'Yes',
+  cancelText: 'No',
+  animation: 'fade'
+});
 
 export default new Vuetify({
   theme: {
