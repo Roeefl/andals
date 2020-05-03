eebbc74c319b9fa331484a42f26b2f20d3b82f96
Roee FL <roeefl@gmail.com>
2020-05-03 17:04:53 +0300

    Remove white walker wildling type until further notice

--- src/specs/wildlings.js
+++ src/specs/wildlings.js
@@ -1,18 +1,18 @@
 import tileColors from '@/styles/export.scss';
 
-const wildlingTypes = ['regular', 'climber', 'giant', 'whiteWalker']
-export const [WILDLING_REGULAR, WILDLING_CLIMBER, WILDLING_GIANT, WILDLING_WHITE_WALKER] = wildlingTypes;
+const wildlingTypes = ['regular', 'climber', 'giant']; //, 'whiteWalker']
+export const [WILDLING_REGULAR, WILDLING_CLIMBER, WILDLING_GIANT] = wildlingTypes; // , WILDLING_WHITE_WALKER] = wildlingTypes;
 
 export const wildlingIcons = {
   regular: 'gavel',
   climber: 'pickaxe',
   giant: 'football-helmet',
-  whiteWalker: 'ghost'
+  // whiteWalker: 'ghost'
 };
 
 export const wildlingColors = {
   regular: tileColors.mountain,
   climber: tileColors.mountain,
   giant: tileColors.secondary,
-  whiteWalker: tileColors.secondary
+  // whiteWalker: tileColors.secondary
 };
