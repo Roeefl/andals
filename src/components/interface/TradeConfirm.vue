8c8db760ca1faed834283d34d54d114fb205b22a
Roee FL <roeefl@gmail.com>
2020-05-13 13:35:19 +0300

    TradeConfirm redesign to small simple request dialog
    When someone asks you directly for a trade, it should not be displayed on a full-screen overlay and be intrusive/blocking, something nicer, simpler

--- src/components/interface/TradeConfirm.vue
+++ src/components/interface/TradeConfirm.vue
@@ -1,44 +1,14 @@
-<template>
-  <v-dialog
-    :value="isOpen"
-    persistent
-    width="500"
-  >
-    <ActionCard :title="title" cancelText="Refuse" approveText="Agree" @cancel="$emit('no')" @approve="$emit('yes')">
-      <h2 v-if="!requestedResource" key="direct-trade">
-        Confirm Trade with {{ withWho }}
-      </h2>
-      <div v-else key="requested-trade" class="requested-resource">
-        <h3>
-          {{ withWho }} is asking if anyone is willing to trade a
-        </h3>
-        <div class="resource-card-wrapper">
-          <ResourceCard
-            hideCount
-            :resource="requestedResource"
-            @clicked="onResourceClick(resource)"
-          ></ResourceCard>
-        </div>
-      </div>
-    </ActionCard>
-  </v-dialog>
-</template>
-
 <script>
-  import ActionCard from '@/components/common/ActionCard';
+  import BaseButton from '@/components/common/BaseButton';
   import ResourceCard from '@/components/game/ResourceCard';
 
   export default {
     name: 'TradeConfirm',
     components: {
-      ActionCard,
-      ResourceCard
+      BaseButton,
+      ResourceCard,
     },
     props: {
-      isOpen: {
-        type: Boolean,
-        default: false
-      },
       withWho: {
         type: String,
         default: ''
@@ -46,22 +16,70 @@
       requestedResource: {
         type: String,
         default: null
-      },
-      title: {
-        type: String,
-        default: 'Confirm Trade'
       }
     }
   }
 </script>
 
+<template>
+  <div class="trade-confirm">
+    <h3 v-if="!requestedResource" key="direct-trade">
+      {{ withWho }} is asking to trade with you
+    </h3>
+    <div v-else key="requested-trade" class="requested-resource">
+      <h3>
+        {{ withWho }} is asking if anyone is willing to trade a
+      </h3>
+      <div class="resource-card-wrapper">
+        <ResourceCard
+          hideCount
+          :resource="requestedResource"
+          @clicked="onResourceClick(resource)"
+        ></ResourceCard>
+      </div>
+    </div>
+    <div class="confirm-actions">
+      <BaseButton
+        icon
+        iconName="cancel"
+        iconColor="red"
+        iconSize="40px"
+        @click="$emit('no')"
+      />
+      <BaseButton
+        icon
+        iconName="check"
+        iconSize="40px"
+        iconColor="light-green darken-2"
+        @click="$emit('yes')"
+      />
+    </div>
+  </div>
+</template>
+
 <style scoped lang="scss">
   @import '@/styles/partials';
-  
-  .requested-resource {
+
+  .trade-confirm {
     padding: $spacer;
+    width: 240px;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    background: $secondary;
+    opacity: 0.8;
+    z-index: $zindex-screen-overlay;
+    border-radius: 20px;
+
+    .requested-resource {
+      padding: $spacer;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .confirm-actions {
+      display: flex;
+      justify-content: space-evenly;
+    }
   }
 </style>
